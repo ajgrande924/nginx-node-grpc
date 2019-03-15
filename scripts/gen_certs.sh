d6fba025ac67b9ed3326086f9674a8fe3ce0ca65
AJ Grande <ajgrande924@gmail.com>
2019-03-15 10:16:18 -0700

    Change configs for rtlgen.arteris.com

--- scripts/gen_certs.sh
+++ scripts/gen_certs.sh
@@ -9,7 +9,7 @@ openssl req -passin pass:1111 -new -x509 -days 365 -key ca.key -out ca.crt -subj
 
 openssl genrsa -passout pass:1111 -des3 -out server.key 4096
 
-openssl req -passin pass:1111 -new -key server.key -out server.csr -subj  "/C=CL/ST=RM/L=Santiago/O=Test/OU=Server/CN=localhost"
+openssl req -passin pass:1111 -new -key server.key -out server.csr -subj  "/C=CL/ST=RM/L=Santiago/O=Test/OU=Server/CN=rtlgen.arteris.com" # /CN=localhost
 
 openssl x509 -req -passin pass:1111 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 
@@ -17,7 +17,7 @@ openssl rsa -passin pass:1111 -in server.key -out server.key
 
 openssl genrsa -passout pass:1111 -des3 -out client.key 4096
 
-openssl req -passin pass:1111 -new -key client.key -out client.csr -subj  "/C=CL/ST=RM/L=Santiago/O=Test/OU=Client/CN=localhost"
+openssl req -passin pass:1111 -new -key client.key -out client.csr -subj  "/C=CL/ST=RM/L=Santiago/O=Test/OU=Client/CN=rtlgen.arteris.com" # /CN=localhost
 
 openssl x509 -passin pass:1111 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
 
